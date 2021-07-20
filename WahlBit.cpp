8648354e5d669b3862f64cfa08ec5df3733a3bc1
Christopher Wahl <cmwahl@hotmail.com>
2021-07-19 22:10:37 -0500

    Got rid of the subdirectory

---
+++ WahlBit.cpp
@@ -0,0 +1,32 @@
+﻿// WahlBit.cpp : Defines the entry point for the application.
+//
+
+#include "WahlBit.h"
+
+namespace WahlBit {
+	
+	BitsParser::BitsParser(void* _data, unsigned int _dataLength, unsigned int _bufferLength) {
+		data = (char*)_data;
+		dataLength = _dataLength;
+		bufferLength = _bufferLength;
+		buffer = (char*)malloc(bufferLength);
+
+	}
+
+	BitsParser::~BitsParser() {
+		free(buffer);
+	}
+
+	void* BitsParser::getBits(int numberOfBits) {
+		copyToBuffer();
+
+		unsigned short totalShift = 0;
+
+		return nullptr;
+	}
+
+	void BitsParser::copyToBuffer() {
+		memcpy(buffer, data + bytesLoc, bufferLength);
+	}
+
+}
